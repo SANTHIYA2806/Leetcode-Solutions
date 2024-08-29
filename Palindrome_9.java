cc7df22a761a04965bf3eaaee7331c3736def800
SANTHIYA2806 <138847027+SANTHIYA2806@users.noreply.github.com>
2024-08-29 22:12:15 +0530

    Create Palindrome_9.java

---
+++ Palindrome_9.java
@@ -0,0 +1,17 @@
+class Palindrome_9{
+    public boolean isPalindrome(int x){
+        int temp = x;
+        int rev = 0,rem;
+        while(x>0){
+            rem = x%10;
+            rev = rev*10+rem;
+            x = x/10;
+        }
+        if(rev==temp){
+            return true;
+        }
+        else{
+            return false;
+        }
+    }
+}
