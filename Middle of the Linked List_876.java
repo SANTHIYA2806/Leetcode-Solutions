27a3a867a75cf7e662529c0f6a627043fba72751
SANTHIYA2806 <138847027+SANTHIYA2806@users.noreply.github.com>
2025-02-08 21:00:42 +0530

    Update Middle of the Linked List_876.java

--- Middle of the Linked List_876.java
+++ Middle of the Linked List_876.java
@@ -20,3 +20,36 @@ class Solution {
        return slow;
     }
 }
+
+/*
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+class Solution {
+    public ListNode middleNode(ListNode head) {
+        ListNode temp = head;
+        int count = 0;
+        while(temp!=null){
+            count++;
+            temp = temp.next;
+        }
+        int middle = (count/2) + 1;
+        temp = head;
+        while(temp!=null){
+            middle += - 1;
+            if(middle != 0){
+                temp  =temp.next;
+            }
+            else{
+                break;
+            }
+        }
+        return temp;
+    }
+}
+*/
